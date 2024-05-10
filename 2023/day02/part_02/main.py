734fdbe89ac96bfc0059a07cc26328e34e5ed9ab
thisiskennyb <kendbonnette@gmail.com>
2024-05-10 14:24:13 -0400

    solved 2023 day02 part2

--- 2023/day02/part_02/main.py
+++ 2023/day02/part_02/main.py
@@ -11,36 +11,46 @@ def extract_random_cubes(game):
     matches = re.findall(cube_pattern, game)
     color_number_tuples = [(match[1], int(match[0])) for match in matches]
     return color_number_tuples
+
+def find_product_of_set_values(set):
+    product = 1
+    for value in set.values():
+        product *= int(value)  
+    return product
     
-def determine_valid_game(game):
+def find_power_of_set(game):
     '''
     Return Game ID value if game is valid
     Else Return 0
     '''
-    valid_game_params = {
-        "red": 12,
-        "green": 13,
-        "blue": 14,       
+    cube_counts = {
+        "red": None,
+        "green": None,
+        "blue": None
     }
     
-    game_id = extract_game_id(game)
     random_cubes = extract_random_cubes(game)
     
     for cube in random_cubes:
-        if valid_game_params[cube[0]] < int(cube[1]):
-            return 0
+        color, number = cube
+        if not cube_counts[color]:
+            cube_counts[color] = int(number)
+        elif cube_counts[color] < int(number):
+            cube_counts[color] = int(number)
+            
+    set_power = find_product_of_set_values(cube_counts)
     
-    return game_id
+    return set_power
 
 
-def find_game_id_sum(file):
+def find_sum_of_powers(file):
     valid_game_id_sum = 0
     
     with open(file, "r") as infile:
         for line in infile:
             line.strip()
-            valid_game_id_sum += determine_valid_game(line)
+            valid_game_id_sum += find_power_of_set(line)
             
     return valid_game_id_sum
             
-print(find_game_id_sum("puzzle.txt"))
+print(find_sum_of_powers("puzzle.txt"))
